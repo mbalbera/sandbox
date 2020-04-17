b59948d0c47b2b546208ab3dfcea6b7f9f34be42
Mason Balbera <mhbalbera@gmail.com>
2020-04-17 12:52:06 -0400

    initial setup

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import HomeScreen from './screens/homeScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+  
+  render(){
+    return (
+      <View style={styles.container}>
+        <HomeScreen/>
+      </View>
+    );
+  }
 }
 
+export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ screens/homeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default HomeScreen = ()=>{
+    render(
+        <View>
+            <View>
+                <Text>HomeScreen</Text>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
